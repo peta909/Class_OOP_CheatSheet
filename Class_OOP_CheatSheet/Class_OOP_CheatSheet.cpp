c6dd2f42facc66a0d13322cda512deea52c8112a
Mark Lim <peta909@hotmail.com>
2019-02-21 17:34:57 -0800

    1st upload

--- Class_OOP_CheatSheet/Class_OOP_CheatSheet.cpp
+++ Class_OOP_CheatSheet/Class_OOP_CheatSheet.cpp
@@ -1,21 +1,63 @@
 // Class_OOP_CheatSheet.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include "pch.h"
 #include <iostream>
+#include <string>
+using namespace std;
+
+class Fighters {
+private:
+	int Stealth;
+
+public:
+	string model;
+	int Health;
+	int Strength;
+	void add_stealth();
+	void printinfo();
+
+	Fighters() {
+		model = "Default Model";
+		Health = 50;
+		Strength = 50;
+		Stealth = 0;
+	}
+};
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	Fighters F16;
+	Fighters* pF16 = &F16;
+	pF16->model = "F16_TG";
+	pF16->Health = 100;//Heath is updated but Strength is default
+	//F16.printinfo(); less perferred way to access to functions in class
+	pF16->printinfo();
+
+	Fighters F35;
+	Fighters* pF35 = &F35;
+	pF35->model = "F35_NG";
+	pF35->Health = 100;
+	pF35->Strength = 80;
+	//pF35->Stealth = 100;//Stealth is a private variable in the Fighters class
+	pF35->add_stealth();//public function that have access to private variable Stealth
+	pF35->printinfo();
+
+	Fighters* pJ10 = new Fighters; // assigned in Heap instead of stack
+	pJ10->add_stealth();
+	pJ10->printinfo();
+
+
+	getchar();
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void Fighters::printinfo() {
+	cout << "\nFighter: " << model << endl;
+	cout << "Health is " << Health << endl;
+	cout << "Strength is " << Strength << endl;
+	cout << "Stealth is " << Stealth << endl;
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void Fighters::add_stealth() {
+	Stealth = Stealth + 10;
+}
